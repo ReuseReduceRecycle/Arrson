20682d2dcc7a43dbbc7d1bb6c843f99c024964f4
Walid <reusereducerecycle@botnets.me>
2017-09-17 13:31:12 -0400

    Add base provider

---
+++ Providers/IProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using CodeHollow.FeedReader;
+
+namespace Providers
+{
+    internal interface IProvider
+    {
+        string SearchTerm { set; }
+
+        Task<Feed> Run();
+    }
+}
