3f995af7a294b172834754064ca2c3c2fceb6d5b
Walid <reusereducerecycle@botnets.me>
2017-09-17 13:31:25 -0400

    Add Nyaa provider

---
+++ Providers/Nyaa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+using System.Web;
+using CodeHollow.FeedReader;
+
+namespace Providers
+{
+    internal class Nyaa : IProvider
+    {
+        public Nyaa()
+        {
+            Uri = new UriBuilder(Url);
+
+            QueryDictionary = HttpUtility.ParseQueryString(Uri.Query);
+
+            QueryDictionary["page"] = "rss";
+        }
+
+        private NameValueCollection QueryDictionary { get; }
+
+        private UriBuilder Uri { get; }
+
+        private static string Url => "https://nyaa.si/";
+
+        public string SearchTerm
+        {
+            set => QueryDictionary["q"] = value;
+        }
+
+        public async Task<Feed> Run()
+        {
+            Uri.Query = QueryDictionary.ToString();
+
+            Console.WriteLine(Uri.ToString());
+
+            return await FeedReader.ReadAsync(Uri.ToString());
+        }
+    }
+}
